d5da01cf72795ed6c4c1d94c46d4cc043d00d2ba
Надира Тургуналиева <1132226224@pfur.ru>
2024-05-21 12:01:30 +0300

    класс Shapr

---
+++ laba3_s4/Shape.h
@@ -0,0 +1,11 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <string>
+
+class Shape {
+public:
+	virtual void Scale(float scaleFactor) = 0;
+	virtual void ShowInfo() = 0;
+	virtual std::string GetName() = 0;
+};
+#endif SHAPE_H
